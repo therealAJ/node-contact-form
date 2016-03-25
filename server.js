2d6e40d29babb2d116576cc32a892b7976bc741c
Alex Jordache <alexonej@gmail.com>
2016-03-25 00:55:16 -0700

    add nodemailer smtp

--- server.js
+++ server.js
@@ -3,9 +3,44 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+var smtpTransport = nodemailer.createTransport("SMTP", {
+
+    service: 'Gmail',
+    auth: {
+        user: "",
+        pass: ""
+    }
+});
+
 
 
 app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+    res.sendfile('./public/index.html');
+});
+
+app.get('/send', function (req, res) {
+
+    var mailOptions = {
+        name: req.query.name,
+        to: req.query.to,
+        text: req.query.text
+    }
+
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function (err, response) {
+        if (err) {
+            console.log(err);
+            res.end("error");
+        } else {
+            console.log("Message sent: " + response.message);
+            res.end("sent");
+        }
+    });
+
+});
 
 
 app.listen(8080, function (err) {
@@ -14,8 +49,4 @@ app.listen(8080, function (err) {
     } else {
         console.log("Listening on port on 8080");
     }
-});
-
-app.get('/', function (req, res) {
-    res.sendfile('./public/index.html');
 });
